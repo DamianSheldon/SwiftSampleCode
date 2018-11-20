9ee738979aeaf22c01ab16b21cea259cbbf30912
Meiliang Dong <dongmeilianghy@sina.com>
2018-11-20 20:55:13 +0800

    Center image in scroll view

--- Oxygen/Oxygen/ImageScrollView.swift
+++ Oxygen/Oxygen/ImageScrollView.swift
@@ -46,5 +46,27 @@ open class ImageScrollView: UIScrollView {
             
             addSubview(frontTiledImageView)
         }
+        
+        // center the image as it becomes smaller than the size of the screen
+        let boundsSize = bounds.size
+        var frameToCenter = frontTiledImageView.frame
+        
+        // center horizontally
+        if frameToCenter.size.width < boundsSize.width {
+            frameToCenter.origin.x = (boundsSize.width - frameToCenter.size.width) / 2.0
+        }
+        else {
+            frameToCenter.origin.x = 0
+        }
+        
+        // center vertically
+        if frameToCenter.size.height < boundsSize.height {
+            frameToCenter.origin.y = (boundsSize.height - frameToCenter.size.height) / 2.0
+        }
+        else {
+            frameToCenter.origin.y = 0
+        }
+        
+        frontTiledImageView.frame = frameToCenter
     }
 }
